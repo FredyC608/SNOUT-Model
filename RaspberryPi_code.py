6124dbe5134c303d348b4fa6ed49ba48df3676e7
CalebSnider <calebm0815@tamu.edu>
2025-11-06 22:01:41 -0600

    Only instructions to set connect to pi, as well as change the hotspot it is connected to

--- RaspberryPi_code.py
+++ RaspberryPi_code.py
@@ -1,3 +1,33 @@
 
 
+#To connect to pi:
+#ssh pi@snout.local
+#username snout
+
+#To change the hotspot being used (allegedly):
+#sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+#network={
+#    ssid="NewHotspotName"
+#    psk="NewPassword"
+#}
+#
+#then
+#sudo reboot
+
+
+
+#current:
+#network={
+#    ssid="iPhone (3323)"
+#    psk="snout123"
+#}
+
+
+
+
+
+
+
+
+
 
